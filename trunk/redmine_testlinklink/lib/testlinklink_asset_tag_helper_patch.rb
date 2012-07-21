bc7466dc1655b0628466b4f601d0262360d64331
myoshida <myoshida@dc647a54-03a9-4fda-b213-c1470a891d78>
2012-07-21 23:35:00 +0000

    refs #1055 : Renames extension from ".rhtml" to "html.erb" for Rails 3.

--- trunk/redmine_testlinklink/lib/testlinklink_asset_tag_helper_patch.rb
+++ trunk/redmine_testlinklink/lib/testlinklink_asset_tag_helper_patch.rb
@@ -1,23 +1,39 @@
-module ActionView
-  module Helpers
-    module AssetTagHelper
-      def javascript_include_tag_with_testlinklink(*sources)
-        out = javascript_include_tag_without_testlinklink(*sources)
-        if sources.is_a?(Array) and sources[0] == 'jstoolbar/textile'
-          out += javascript_tag <<-javascript_tag
-jsToolBar.prototype.elements.testcase = {
-	type: 'button',
-        title: '#{l(:label_tag_testcase)}',
-	fn: {
-		wiki: function() { this.encloseSelection("{{testcase(", ")}}") }
-	}
-}
-javascript_tag
-          out += stylesheet_link_tag 'testcase', :plugin => 'redmine_testlinklink'
+require_dependency "redmine/wiki_formatting/textile/helper"
+
+module Redmine
+  module WikiFormatting
+    module Textile
+      module Helper
+
+        def self.included(base)
+          base.class_eval do
+            unloadable # Send unloadable so it will not be unloaded in development    
+            alias_method_chain :heads_for_wiki_formatter, :testlinklink
+          end
         end
-        out
+
+        
+        def heads_for_wiki_formatter_with_testlinklink
+          heads_for_wiki_formatter_without_testlinklink
+          unless @heads_for_testlinklink_included
+            content_for :header_tags do
+              out = stylesheet_link_tag("testcase.css", :plugin => "redmine_testlinklink")
+              out += javascript_tag <<-javascript_tag
+              jsToolBar.prototype.elements.testcase = {
+                type: 'button',
+                title: '#{l(:label_tag_testcase)}',
+                fn: {
+                  wiki: function() { this.encloseSelection("{{testcase(", ")}}") }
+                }
+              }
+              javascript_tag
+              out
+            end
+            @heads_for_testlinklink_included = true
+          end
+        end
+        
       end
-      alias_method_chain :javascript_include_tag, :testlinklink
     end
   end
 end
